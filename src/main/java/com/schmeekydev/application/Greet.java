f8835e383f7dc5658f98dec94af0794b41e297f2
Abdul Samad Shaikh <Abdul Samad Shaikh>
2021-10-21 14:04:41 +0530

    explicitly added discrete functions

---
+++ src/main/java/com/schmeekydev/application/Greet.java
@@ -0,0 +1,33 @@
+package com.schmeekydev.application;
+
+import java.util.Random;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Greet implements MessageCreateListener {
+    private static final Logger clog = LogManager.getLogger(Greet.class);
+
+    @Override
+    public void onMessageCreate(MessageCreateEvent event) {
+        String[] text = event.getMessage().getContent().split(" ");
+        try {
+            if (text[0].equalsIgnoreCase("hi") || text[0].equals("hello")) {
+                event.getChannel().sendMessage(sayHello());
+                System.out.println("Triggerrrrred");
+                clog.fatal(String.format("Replied with "));
+            }
+        } catch (Exception exception) {
+            System.out.println("Error");
+            clog.fatal("An error occured\nCannot print full stack trace for the error");
+        }
+    }
+
+    public static String sayHello() {
+        Random randomGenerator = new Random();
+        String[] greetings = { "Marhaba", "Namaskar", "Hola", "Bonjour", "Guten Tag", "Shalom", "Salve",
+                "Konnichiwa, Senpai >~<\"\"", "Assalamualaikum", "Namaste", "Wadakkam" };
+        return greetings[randomGenerator.nextInt(greetings.length)];
+    }
+}
