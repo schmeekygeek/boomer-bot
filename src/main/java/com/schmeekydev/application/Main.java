972604cd806e6d1f2d1b8197ec7d4fe7edb4d642
Abdul Samad Shaikh <Abdul Samad Shaikh>
2021-10-21 14:02:35 +0530

    Bug fixes and optimizations

--- src/main/java/com/schmeekydev/application/Main.java
+++ src/main/java/com/schmeekydev/application/Main.java
@@ -1,17 +1,34 @@
+//////////////////////////////////////////////////////////////////////////////
+//      Abdul Samad Shaikh                                                  //
+//==========================================================================//
+//                                                                          //
+//      GENERAL PUBLIC LICENSE                                              //
+//      Version 1.0.1, August 2021                                          //
+//      CopyRight (C) 2021, 2021 schmeekydev Productions, Inc.              //
+//      All Rights Reserved                                                 //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
 package com.schmeekydev.application;
 
-import com.schmeekydev.application.throw_Command.joke;
-import com.schmeekydev.application.throw_Command.advice;
-
+import com.schmeekydev.application.throw_Command.Advice;
+import com.schmeekydev.application.throw_Command.Joke;
+import com.schmeekydev.application.throw_Command.Meme;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger clog = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        String token = "ODc5NzI3MDY4ODIzMzAyMjM0.YST7rQ.ws0794nflfJX_Qu_m6qPjxx1H1Y";
+        String token = "ODc5NzI3MDY4ODIzMzAyMjM0.YST7rQ.XfijhlT1y8o4zc8gNGly9URJXTA";
+        clog.info("Application initialized");
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
-        api.addListener(new greet());
-        api.addListener(new joke());
-        api.addListener(new advice());
+        api.addListener(new Greet());
+        api.addListener(new Joke());
+        api.addListener(new Advice());
+        api.addListener(new Meme());
     }
 }
--- src/main/java/com/schmeekydev/application/throw_Command/Meme.java
+++ src/main/java/com/schmeekydev/application/throw_Command/Meme.java
@@ -8,24 +8,22 @@ import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
-public class Meme implements MessageCreateListener{
+public class Meme implements MessageCreateListener {
     @Override
-    public void onMessageCreate(MessageCreateEvent event){
+    public void onMessageCreate(MessageCreateEvent event) {
         Random randomGenerator = new Random();
 
-        String[] text = event.getMessage()
-                            .getContent()
-                            .split(" ");
-        
+        String[] text = event.getMessage().getContent().split(" ");
+
         EmbedBuilder memeEmbed = new EmbedBuilder();
         memeEmbed.setTitle("Here, take a look at this meme.\nHahahaha so funnyy!!!!")
-                .setDescription("I found this on facebook")
-                .setColor(Color.BLUE)
-                .setImage(new File(String.format("src/minionmemes/%s.jpg",Integer.toString(randomGenerator.nextInt(23)))))
-                .setAuthor("My Profile", "https://rr.noordstar.me/ac85117e", "https://cdn.discordapp.com/embed/avatars/0.png");
+                .setDescription("I found this on facebook").setColor(Color.BLUE)
+                .setImage(new File(
+                        String.format("src/minionmemes/%s.jpg", Integer.toString(randomGenerator.nextInt(23)))))
+                .setAuthor("My Profile", "https://rr.noordstar.me/ac85117e",
+                        "https://cdn.discordapp.com/embed/avatars/0.png");
 
-
-        if(text[0].equalsIgnoreCase("throw")&&text[1].equalsIgnoreCase("meme")){
+        if (text[0].equalsIgnoreCase("throw") && text[1].equalsIgnoreCase("meme")) {
             event.getChannel().sendMessage(memeEmbed);
         }
     }
