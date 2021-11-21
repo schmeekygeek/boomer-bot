015db0720a61d4ea75b82c88f9fd0c75084b1254
schmeekygeek <ss6087982@gmail.com>
2021-11-22 01:19:20 +0530

    improved bot security

--- src/main/java/com/schmeekydev/application/Main.java
+++ src/main/java/com/schmeekydev/application/Main.java
@@ -15,20 +15,22 @@ import com.schmeekydev.application.throw_Command.Advice;
 import com.schmeekydev.application.throw_Command.Joke;
 import com.schmeekydev.application.throw_Command.Meme;
 import org.javacord.api.DiscordApi;
-import org       .javacord.api.DiscordApiBuilder;
+import org.javacord.api.DiscordApiBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.io.*;
 
 public class Main {
     private static final Logger clog = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) {
-        String token = "ODc5NzI3MDY4ODIzMzAyMjM0.YST7rQ.XfijhlT1y8o4zc8gNGly9URJXTA";
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufRead = new BufferedReader(new FileReader("token.txt"));
         clog.info("Application initialized");
-        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+        DiscordApi api = new DiscordApiBuilder().setToken(bufRead.readLine()).login().join();
         api.addListener(new Greet());
         api.addListener(new Joke());
         api.addListener(new Advice());
         api.addListener(new Meme());
+        bufRead.close();
     }
 }
